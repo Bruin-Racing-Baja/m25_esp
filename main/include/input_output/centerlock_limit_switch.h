ed3426f04411a3cb813c15f21ff834f12f3f86ec
geeick <109990888+geeick@users.noreply.github.com>
2026-01-03 18:39:17 -0300

    Created basic wrapper for centerlock limit switch

---
+++ main/include/input_output/centerlock_limit_switch.h
@@ -0,0 +1,9 @@
+#ifndef CENTERLOCK_LIMIT_SWITCH_H
+#define CENTERLOCK_LIMIT_SWITCH_H
+
+class CenterlockLimitSwitch {
+public:
+    CenterlockLimitSwitch() {}
+};
+
+#endif // CENTERLOCK_LIMIT_SWITCH_H
