a921b696133f6f205052ec0a2c5d1922dd830f08
sarah darzacq <sarahdarzacq22@gmail.com>
2026-01-03 15:46:56 +0100

    Brake Pot Class Skeleton

---
+++ main/include/sensors/brake_pot_sensor.h
@@ -0,0 +1,34 @@
+#ifndef BRAKE_POT_H
+#define BRAKE_POT_H
+
+#include "sensor.h"
+#include "../macros.h"
+#include <stdint.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/portmacro.h"
+
+class BrakePot: public Sensor {
+public: 
+    BrakePot(uint32_t min_brake_, uint32_t max_brake_): 
+        raw_brake(0),
+        brake(0.0f),
+        min_brake(min_brake_),
+        max_brake(max_brake_)
+        {}
+
+    bool init(); 
+    virtual void update();
+    
+    inline uint32_t get_raw_brake() { return raw_brake; }
+    inline float get_brake() { return map_int_to_float(raw_brake, min_brake, max_brake, 0, 1); }
+
+private:
+    const uint32_t min_brake; 
+    const uint32_t max_brake; 
+
+    uint32_t raw_brake; 
+    float brake; 
+};
+
+#endif // BRAKE_POT_H
---
+++ main/src/sensors/brake_pot_sensor.cpp
@@ -0,0 +1,15 @@
+#include "brake_pot_sensor.h"
+ 
+bool BrakePot::init() {
+    /**
+     * Initialize prper pins as input with correct config.
+    */
+   return true;
+}
+
+void BrakePot::update() {
+    /** 
+     * Read value from brake pot sensor and update private raw_brake field 
+     * with proper value.
+    */
+}
