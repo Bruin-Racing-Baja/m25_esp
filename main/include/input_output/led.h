79628d2e3dacf06d3bd91fa75b78dc8ab1669027
geeick <109990888+geeick@users.noreply.github.com>
2026-01-11 21:04:05 -0800

    Added basic implementations for led wrapper

--- main/include/input_output/led.h
+++ main/include/input_output/led.h
@@ -4,6 +4,14 @@
 class LED {
 public:
     LED() {}
+    void writePin(bool state);
+    void pinMode(int mode);
+    void setFlashTime(int time);
+
+private:
+    int pin;
+    bool led_state;
+    int flash_time;
 };
 
 #endif // LED_H
