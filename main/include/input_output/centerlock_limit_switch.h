e3cdedd91150d07e76eca9510555bd38efcb4a4f
geeick <109990888+geeick@users.noreply.github.com>
2026-01-11 20:30:02 -0800

    Added functions in CenterlockLimitSwitch  wrapper

--- main/include/input_output/centerlock_limit_switch.h
+++ main/include/input_output/centerlock_limit_switch.h
@@ -4,6 +4,16 @@
 class CenterlockLimitSwitch {
 public:
     CenterlockLimitSwitch() {}
+
+    void begin();
+    void update();
+    void end();
+
+private:
+    bool centerlock_limit_switch_activated;
+
+    bool engage_pressed;
+    bool disengage_pressed;
 };
 
 #endif // CENTERLOCK_LIMIT_SWITCH_H
