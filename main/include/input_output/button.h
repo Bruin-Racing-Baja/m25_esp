aa6d1174c06a614592c9a1d31d5cffa19912dd6e
geeick <109990888+geeick@users.noreply.github.com>
2026-01-11 20:56:51 -0800

    Added functions for button wrapper

--- main/include/input_output/button.h
+++ main/include/input_output/button.h
@@ -4,6 +4,18 @@
 class Button {
 public:
     Button() {}
+
+    bool readPin();
+    bool isPressed();
+    bool readLastState();
+
+    bool pressTriggered();
+    bool releaseTriggered();
+
+private:
+    int pin;
+    bool button_pressed;
+    bool last_state;
 };
 
 #endif // BUTTON_H
