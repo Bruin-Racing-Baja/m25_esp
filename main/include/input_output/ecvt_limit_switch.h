48fc20e886418dcbc8cef2eb23cf7b6ddb5563d4
geeick <109990888+geeick@users.noreply.github.com>
2026-01-15 11:40:57 -0800

    Better descriptions and inbound outbound separation for ecvt limit switch functions

--- main/include/input_output/ecvt_limit_switch.h
+++ main/include/input_output/ecvt_limit_switch.h
@@ -6,29 +6,33 @@ public:
     ECVTLimitSwitch() {}
 
     // Returns true if the limit switch is activated
-    bool inbound_triggered() const {
-        return inbound_limit_switch_activated;
+    bool is_inbound_triggered() const {
+        return inbound_limit_switch_activated; 
     }
-    bool outbound_triggered() const {
+    bool is_outbound_triggered() const {
         return outbound_limit_switch_activated;
     }
-    bool engage_triggered() const {
+    bool is_engage_triggered() const {
         return engage_limit_switch_activated;
     }
 
     // Returns true if the switch was pressed
-    bool inbound_pressed() const {
-        return inbound_pressed;
+    bool is_inbound_engaged() const {
+        return inbound_engaged;
     }
-    bool outbound_pressed() const {
-        return outbound_pressed;
+    bool is_outbound_engaged() const {
+        return outbound_engaged;
     }
-    bool engage_pressed() const {
-        return engage_pressed;
+    bool is_engage_engaged() const {
+        return engage_engaged;
     }
     
     void begin();
-    void update();
+
+    void isr_update_inbound();
+    void isr_update_outbound();
+    void isr_update_engage();
+
     void end();
 
 private:
@@ -36,9 +40,9 @@ private:
     bool outbound_limit_switch_activated;
     bool engage_limit_switch_activated;
 
-    bool inbound_pressed;
-    bool outbound_pressed;
-    bool engage_pressed;
+    bool inbound_engaged;
+    bool outbound_engaged;
+    bool engage_engaged;
 };
 
 #endif // ECVT_LIMIT_SWITCH_H
