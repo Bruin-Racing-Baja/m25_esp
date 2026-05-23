0f9aa0b5f4bf6fe3fa089734f9c4f1d1f2af5652
Vincent Krenz <vincentbkrenz@gmail.com>
2026-05-23 08:58:33 -0700

    preemptive velocity limit bug fixes

--- main/include/constants.h
+++ main/include/constants.h
@@ -12,7 +12,7 @@ constexpr uint8_t ECVT_ODRIVE_NODE_ID = 1;
 constexpr uint8_t CENTERLOCK_ODRIVE_NODE_ID = 3; 
 
 /* Odrive Values */
-constexpr float ECVT_ODRIVE_VELOCITY_LIMIT = 30.0f;
+constexpr float ECVT_ODRIVE_VELOCITY_LIMIT = 40.0f;
 constexpr  float ECVT_ODRIVE_CURRENT_LIMIT = 12.5f; 
 constexpr float CENTERLOCK_ODRIVE_VEL_LIMIT = 30.0f; 
 constexpr float CENTERLOCK_ODRIVE_CURRENT_LIMIT = 5.0; 
@@ -32,6 +32,9 @@ constexpr uint32_t CENTERLOCK_BUTTON_DEBOUNCE_MS = 20;
 constexpr float ACTUATOR_KP = 0.01f;   
 constexpr float ACTUATOR_KI = 0.000f;   
 constexpr float ACTUATOR_KD = 0.004f;
+
+constexpr float ECVT_CONTROLLER_INBOUND_VELOCITY_LIMIT = 30.0f; //magnitude
+constexpr float ECVT_CONTROLLER_OUBOUND_VELOCITY_LIMIT = 30.0f; //magnitude
 
 constexpr float ECVT_TARGET_RPM = 3000.0f;
 constexpr float ACTUATOR_INBOUND_THRESHOLD = 8.0f;
